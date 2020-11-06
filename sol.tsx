2b1eaf263be19ed5980842122ddd87635610ea80
DrakenLine <alexandra.farina@mmibordeaux.com>
2020-11-06 15:47:45 +0100

    Test sol

---
+++ sol.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="sol" tilewidth="32" tileheight="32" tilecount="240" columns="16">
+ <image source="../../../assets/rpg_maker_vx__modernrtp_tilea4_by_painhurt_d3f7s8m.png" width="512" height="480"/>
+</tileset>
