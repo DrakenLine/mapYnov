700cbab5836bd24bb7c25d52d96830e2b8a6389e
DrakenLine <alexandra.farina@mmibordeaux.com>
2020-11-06 15:06:42 +0100

    1er push test

---
+++ chemin.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="chemin" tilewidth="32" tileheight="32" tilecount="48" columns="8">
+ <image source="../../../../../../Downloads/Pipoya RPG Tileset 32x32/[A]_type3/[A]Dirt3_pipo.png" width="256" height="192"/>
+ <terraintypes>
+  <terrain name="place" tile="6"/>
+ </terraintypes>
+ <tile id="5" terrain=",,,0"/>
+ <tile id="6" terrain=",,0,0"/>
+ <tile id="7" terrain=",,0,"/>
+ <tile id="13" terrain=",0,,0"/>
+ <tile id="14" terrain="0,0,0,0"/>
+ <tile id="15" terrain="0,,0,"/>
+ <tile id="21" terrain=",0,,"/>
+ <tile id="22" terrain="0,0,,"/>
+ <tile id="23" terrain="0,,,"/>
+</tileset>
