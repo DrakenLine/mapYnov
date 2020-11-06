df4ab753be1829ff930b83245735e3d09661341d
DrakenLine <alexandra.farina@mmibordeaux.com>
2020-11-06 17:54:26 +0100

    End 06/10

---
+++ morgue.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="morgue" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="../../../assets/morgue_by_nicnubill_d6swv2l.png" width="512" height="512"/>
+</tileset>
---
+++ school.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="school" tilewidth="32" tileheight="32" tilecount="420" columns="15">
+ <image source="../../../assets/celianna_schooltiles.png" width="490" height="898"/>
+</tileset>
