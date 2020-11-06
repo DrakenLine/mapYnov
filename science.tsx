12163959f5b3b25ed48e4281a91f271e40d805f6
DrakenLine <43884146+DrakenLine@users.noreply.github.com>
2020-11-06 16:55:06 +0000

    Deploying to gh-pages from master @ df4ab753be1829ff930b83245735e3d09661341d 🚀

---
+++ science.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="science" tilewidth="32" tileheight="32" tilecount="48" columns="12">
+ <image source="../../../assets/mv_scifi_c_addon_by_schwarzenacht_dapf6vj.png" width="384" height="144"/>
+</tileset>
--- sol.tsx
+++ sol.tsx
@@ -1,4 +1,24 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.2" name="sol" tilewidth="32" tileheight="32" tilecount="240" columns="16">
  <image source="../../../assets/rpg_maker_vx__modernrtp_tilea4_by_painhurt_d3f7s8m.png" width="512" height="480"/>
+ <tile id="220">
+  <objectgroup draworder="index" id="5">
+   <object id="5" x="-0.0900647" y="0.180129" width="32.1531" height="31.8829"/>
+  </objectgroup>
+ </tile>
+ <tile id="221">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0900647" y="0.0900647" width="32.1531" height="31.8829"/>
+  </objectgroup>
+ </tile>
+ <tile id="236">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0.0900647" width="31.8829" height="31.8829"/>
+  </objectgroup>
+ </tile>
+ <tile id="237">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.180129" y="0.180129" width="31.8829" height="31.6127"/>
+  </objectgroup>
+ </tile>
 </tileset>
