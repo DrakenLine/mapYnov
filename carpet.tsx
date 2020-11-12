a33795e6b7a384f9db89cb3339fc733dd100660f
DrakenLine <alexandra.farina@mmibordeaux.com>
2020-11-12 10:49:56 +0100

    changement chemin

--- carpet.tsx
+++ carpet.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.2" name="carpet" tilewidth="32" tileheight="32" tilecount="594" columns="33">
- <image source="/Users/alexandrafarina/ALEX/YNOV/STAGE/MAP/startMap/mapYnov.carpet.png" width="1056" height="576"/>
+ <image source="/Users/alexandrafarina/ALEX/YNOV/STAGE/MAP/startMap/mapYnov/carpet.png" width="1056" height="576"/>
 </tileset>
