edd916335d8c3e04fab2be0a83e5bcba53f093f7
DrakenLine <43884146+DrakenLine@users.noreply.github.com>
2020-11-12 08:27:31 +0000

    Deploying to gh-pages from master @ ab060e23f83369b735c1ed6b73e5cc26a1199b5e 🚀

--- carpet.tsx
+++ carpet.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.2" name="carpet" tilewidth="32" tileheight="32" tilecount="594" columns="33">
- <image source="../../../assets/floors_a2_tilesets_rpgmaker_mv_by_alilali_dc6zh6s.png" width="1056" height="576"/>
+ <image source="floors_a2_tilesets_rpgmaker_mv_by_alilali_dc6zh6s.png" width="1056" height="576"/>
 </tileset>
--- test.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="test" tilewidth="32" tileheight="32" tilecount="576" columns="24">
- <image source="../../../assets/hiclipart.com.png" width="768" height="768"/>
-</tileset>
