43853bccbeae3bb84f50c87db609d6c735802873
DrakenLine <43884146+DrakenLine@users.noreply.github.com>
2020-11-12 10:00:38 +0000

    Deploying to gh-pages from master @ d7aa022b5cabbe93ae91e699cfe27cc31ba60f2f 🚀

--- chemin.tsx
+++ chemin.tsx
@@ -1,4 +1,3 @@
-<?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.2" name="chemin" tilewidth="32" tileheight="32" tilecount="48" columns="8">
- <image source="/Users/alexandrafarina/ALEX/YNOV/STAGE/MAP/startMap/mapYnov/chemin.png" width="256" height="192"/>
+ <image source="chemin.png" width="256" height="192"/>
 </tileset>
