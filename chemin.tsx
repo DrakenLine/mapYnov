6b67a4e10dc3b82fd0d38b6473e604391dcc4b10
DrakenLine <alexandra.farina@mmibordeaux.com>
2020-11-12 10:41:28 +0100

    changement chemin

--- chemin.tsx
+++ chemin.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.2" name="chemin" tilewidth="32" tileheight="32" tilecount="48" columns="8">
- <image source="../../../../../../Downloads/Pipoya RPG Tileset 32x32/[A]_type3/[A]Dirt3_pipo.png" width="256" height="192"/>
+ <image source="/Users/alexandrafarina/ALEX/YNOV/STAGE/MAP/startMap/mapYnov/chemin.png" width="256" height="192"/>
 </tileset>
--- sol.tsx
+++ sol.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.2" name="sol" tilewidth="32" tileheight="32" tilecount="240" columns="16">
- <image source="rpg_maker_vx__modernrtp_tilea4_by_painhurt_d3f7s8m.png" width="512" height="480"/>
+ <image source="sol.png" width="512" height="480"/>
  <tile id="220">
   <objectgroup draworder="index" id="5">
    <object id="5" x="-0.0900647" y="0.180129" width="32.1531" height="31.8829"/>
