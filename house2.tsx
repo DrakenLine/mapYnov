45fc4b15a4ed3c1f391903c6bb20c8a0e7919d5a
DrakenLine <43884146+DrakenLine@users.noreply.github.com>
2020-11-12 08:20:01 +0000

    Deploying to gh-pages from master @ 4965f06496032de67bd906adccceffafe2796fda 🚀

--- house2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="house2" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="../../../assets/tilesets_interior_para_rmvx_ace___editados_by_milkian_daiujx6.png" width="512" height="512"/>
-</tileset>
