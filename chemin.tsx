cd5624c177771e989f4be18cd54fb8165c80c841
DrakenLine <alexandra.farina@mmibordeaux.com>
2020-11-18 15:03:20 +0100

    Ajout labo 1

--- chemin.tsx
+++
@@ -1,3 +0,0 @@
-<tileset version="1.4" tiledversion="1.4.2" name="chemin" tilewidth="32" tileheight="32" tilecount="48" columns="8">
- <image source="chemin.png" width="256" height="192"/>
-</tileset>
