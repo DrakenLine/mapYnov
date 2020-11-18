38141d5b05a32498093594d9befa8bc5be47b9fa
DrakenLine <43884146+DrakenLine@users.noreply.github.com>
2020-11-18 13:40:00 +0000

    Deploying to gh-pages from master @ 1b40403c23af9d12eef48e1bbcb07cc4737e16d9 🚀

--- labo2.tsx
+++ labo2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="labo2" tilewidth="32" tileheight="32" tilecount="15" columns="5">
- <image source="labo2.png" width="168" height="124"/>
+<tileset version="1.4" tiledversion="1.4.2" name="labo2" tilewidth="32" tileheight="32" tilecount="20" columns="4">
+ <image source="labo2.png" width="129" height="129"/>
 </tileset>
