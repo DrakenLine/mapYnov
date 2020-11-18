0219ddbbf1952e796b64d58267b5bdd9f77f0d1e
DrakenLine <alexandra.farina@mmibordeaux.com>
2020-11-18 15:08:22 +0100

    Ajout labo 1

--- labo1.tsx
+++ labo1.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="labo1_Plan de travail 1 copie" tilewidth="32" tileheight="32" tilecount="9" columns="3">
- <image source="labo1_Plan de travail 1 copie.png" width="97" height="97"/>
+<tileset version="1.4" tiledversion="1.4.2" name="labo1" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="labo1.png" width="129" height="129"/>
 </tileset>
