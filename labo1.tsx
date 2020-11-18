1b40403c23af9d12eef48e1bbcb07cc4737e16d9
DrakenLine <alexandra.farina@mmibordeaux.com>
2020-11-18 14:39:18 +0100

    fix v1

--- labo1.tsx
+++ labo1.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.2" name="labo1" tilewidth="32" tileheight="32" tilecount="16" columns="8">
- <image source="labo1.png" width="256" height="92"/>
+ <image source="labo1.png" width="256" height="76"/>
 </tileset>
